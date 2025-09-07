0bbe455e8f22c29252e3a42bec0ee7b4b3334d34
Neesha <nishabhattarai778@gmail.com>
2025-09-07 23:55:32 +0545

    frontend updated

--- frontend/src/assets/Component/Navbar.jsx
+++ frontend/src/assets/Component/Navbar.jsx
@@ -39,6 +39,12 @@ export default function Navbar() {
         >
           Cart
         </Link>
+         <Link
+          to="/addproduct"
+          className="hover:text-yellow-400 transition-colors duration-200"
+        >
+          Cart
+        </Link>
         <button
           onClick={handleLogout}
           className="!bg-red-500 hover:bg-red-600 px-4 py-2 rounded-lg font-medium transition-colors duration-200"
