1bdbdba974e96f0fbe68a6253112b9ab7a11080f
Neesha <120797206+neeshabhattarai@users.noreply.github.com>
2025-09-08 01:10:01 +0545

    Update cart.js

--- backend/Db/cart.js
+++ backend/Db/cart.js
@@ -1,15 +1,21 @@
-const mongoose=require("mongoose");
-const cart=new mongoose.Schema({
-   quantity:{
-    type:Number,
-    required:true
-   },
-   productid:{
-    type:mongoose.Schema.ObjectId,
-    ref:"item",
-    required:true,
-   }
+const mongoose = require("mongoose");
 
-})
-const cartModel=mongoose.model("cart",cart);
-module.exports=cartModel;
+const cart = new mongoose.Schema({
+   quantity: {
+       type: Number,
+       required: true
+   },
+   productid: {
+       type: mongoose.Schema.ObjectId,
+       ref: "item",
+       required: true,
+   },
+   userid: {
+       type: mongoose.Schema.ObjectId,
+       ref: "user", // Make sure your user model is named "user"
+       required: true
+   }
+});
+
+const cartModel = mongoose.model("cart", cart);
+module.exports = cartModel;
